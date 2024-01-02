93d90a495d009d414a8327aef4d4e4c78e530f19
Mithat Akgün <bubu000051@gmail.com>
2024-01-02 10:37:54 +0100

    saaass

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! du oller benno, jetzt aber.. gggg.</Text>
+      <Text>Open up App.js to start working on your app! du oller benvvno, jetzt aber.. gggg.</Text>
       <StatusBar style="auto" />
     </View>
   );
